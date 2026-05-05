cdac1148096d31b8f49657ad93d20339e57320b0
Thokozile123 <36793963+Thokozile123@users.noreply.github.com>
2026-05-05 14:54:20 +0200

    Update Streamlit.app.py

--- Streamlit.app.py
+++ Streamlit.app.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import nltk
 import gensim
 import bs4 as bs
-#import texthero as hero
+import texthero as hero
 
 from patsy import dmatrices
 import statsmodels.api as sm
